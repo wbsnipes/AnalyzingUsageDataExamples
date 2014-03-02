238ef436d69e33c032cb233d3f52aa406aa0dfc8
emerson <emerson@toolcita2>
2014-03-02 17:27:56 -0500

    Removed some cruft

--- edu.ncsu.csc.dlf.BasicUDCServer/src/edu/ncsu/csc/dlf/BasicUDCServer/BasicUDCServer.java
+++ edu.ncsu.csc.dlf.BasicUDCServer/src/edu/ncsu/csc/dlf/BasicUDCServer/BasicUDCServer.java
@@ -1,20 +1,11 @@
 package edu.ncsu.csc.dlf.BasicUDCServer;
 
 import java.io.IOException;
-import java.io.InterruptedIOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 
-import org.apache.http.ConnectionClosedException;
-import org.apache.http.HttpConnectionFactory;
 import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpException;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpServerConnection;
-import org.apache.http.impl.DefaultBHttpServerConnection;
-import org.apache.http.impl.DefaultBHttpServerConnectionFactory;
-import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.protocol.HttpProcessor;
 import org.apache.http.protocol.HttpProcessorBuilder;
@@ -31,9 +22,6 @@ import org.apache.http.util.EntityUtils;
  * Based on
  * http://hc.apache.org/httpcomponents-core-ga/httpcore/examples/org/apache
  * /http/examples/ElementalHttpServer.java
- * 
- * 
- * @author emerson
  */
 public class BasicUDCServer {
 
@@ -43,8 +31,7 @@ public class BasicUDCServer {
 
 		HttpProcessor httpproc = HttpProcessorBuilder.create()
 				.add(new ResponseDate()).add(new ResponseServer())
-				.add(new ResponseContent()).add(new ResponseConnControl())
-				.build();
+				.add(new ResponseContent()).add(new ResponseConnControl()).build();
 
 		UriHttpRequestHandlerMapper reqistry = new UriHttpRequestHandlerMapper();
 		reqistry.register("*", new HttpRequestHandler() {
@@ -53,15 +40,14 @@ public class BasicUDCServer {
 			public void handle(HttpRequest request, HttpResponse response,
 					HttpContext context) throws HttpException, IOException {
 
+				HttpEntityEnclosingRequest entityRequest = (HttpEntityEnclosingRequest) request;
 
-				HttpEntityEnclosingRequest entityRequest = (HttpEntityEnclosingRequest)request;
-				
 				String userID = request.getHeaders("USERID")[0].getValue();
 				String workspaceID = request.getHeaders("WORKSPACEID")[0].getValue();
 				long time = Long.parseLong(request.getHeaders("TIME")[0].getValue());
 
 				System.out.println(userID + "," + workspaceID + "," + time);
-				System.out.println(EntityUtils.toString(entityRequest.getEntity()));		
+				System.out.println(EntityUtils.toString(entityRequest.getEntity()));
 			}
 		});
 
@@ -70,84 +56,5 @@ public class BasicUDCServer {
 		Thread t = new RequestListenerThread(port, httpService);
 		t.setDaemon(false);
 		t.start();
-	}
-
-	static class RequestListenerThread extends Thread {
-
-		private final HttpConnectionFactory<DefaultBHttpServerConnection> connFactory;
-		private final ServerSocket serversocket;
-		private final HttpService httpService;
-
-		public RequestListenerThread(final int port,
-				final HttpService httpService) throws IOException {
-			this.connFactory = DefaultBHttpServerConnectionFactory.INSTANCE;
-			this.serversocket = new ServerSocket(port);
-			this.httpService = httpService;
-		}
-
-		@Override
-		public void run() {
-			System.out.println("Listening on port "
-					+ this.serversocket.getLocalPort());
-			while (!Thread.interrupted()) {
-				try {
-					// Set up HTTP connection
-					Socket socket = this.serversocket.accept();
-					System.out.println("Incoming connection from "
-							+ socket.getInetAddress());
-					HttpServerConnection conn = this.connFactory
-							.createConnection(socket);
-
-					// Start worker thread
-					Thread t = new WorkerThread(this.httpService, conn);
-					t.setDaemon(true);
-					t.start();
-				} catch (InterruptedIOException ex) {
-					break;
-				} catch (IOException e) {
-					System.err
-							.println("I/O error initialising connection thread: "
-									+ e.getMessage());
-					break;
-				}
-			}
-		}
-	}
-
-	static class WorkerThread extends Thread {
-
-		private final HttpService httpservice;
-		private final HttpServerConnection conn;
-
-		public WorkerThread(final HttpService httpservice,
-				final HttpServerConnection conn) {
-			super();
-			this.httpservice = httpservice;
-			this.conn = conn;
-		}
-
-		@Override
-		public void run() {
-			System.out.println("New connection thread");
-			HttpContext context = new BasicHttpContext(null);
-			try {
-				while (!Thread.interrupted() && this.conn.isOpen()) {
-					this.httpservice.handleRequest(this.conn, context);
-				}
-			} catch (ConnectionClosedException ex) {
-				System.err.println("Client closed connection");
-			} catch (IOException ex) {
-				System.err.println("I/O error: " + ex.getMessage());
-			} catch (HttpException ex) {
-				System.err.println("Unrecoverable HTTP protocol violation: "
-						+ ex.getMessage());
-			} finally {
-				try {
-					this.conn.shutdown();
-				} catch (IOException ignore) {
-				}
-			}
-		}
-
 	}
 }
